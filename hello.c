30cd15c62a3e6a5269dff1f690ab8e46c49de20d
MonitKapoor <kapoor.monit@gmail.com>
2025-07-24 11:22:54 +0530

    committing changes to hello.c,comments added

--- hello.c
+++ hello.c
@@ -1,3 +1,9 @@
+//this is a hello world program
 #include<stdio.h>
 int main()
-{}
+//main begins	
+{
+printf("this isa demo of  C");
+printf("helloworld");//helloworld added
+return 0;//return statement for succesful exit from main
+}
